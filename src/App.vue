a572f39aedcfea3de44fdf257093b09c8e0b7eaf
Adam Jagosz <adamjagosz@gmail.com>
2019-09-04 10:07:00 +0200

    add scss and UiRadioGroup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UiRadioGroup
+      name="radio-1"
+      v-model="value"
+      :options="options"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UiRadioGroup from "keen-ui/src/UiRadioGroup.vue";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    UiRadioGroup
+  },
+  data() {
+    return {
+      value: "",
+      options: [
+        { value: "", label: "default" },
+        { value: "a", label: "option a" },
+        { value: "v", label: "option b" },
+      ],
+    };
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
